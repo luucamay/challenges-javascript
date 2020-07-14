27b8d0261d54b3fd00f2d5997de670a3adf97dc2
Lupe CaMay <lupemaydana@gmail.com>
2020-07-14 09:06:46 -0400

    Update vowel-count.js

--- vowel-count.js
+++ vowel-count.js
@@ -1,7 +1,11 @@
 function getCount(str) {
   var vowelsCount = 0;
+  const vowels = ['a','e','i','o','u'];
   
-  // enter your majic here
+  for (let i =0; i<str.length; i++){
+   if (vowels.includes(str[i]))
+     vowelsCount++;
+  }
   
   return vowelsCount;
 }
